16c7335ab04821869eb4f6b285e2ec7884428d82
mbasso <mbasso96@gmail.com>
2017-02-25 22:01:58 +0100

    VNode as struct

--- src/cpp/VNode/VNode.hpp
+++ src/cpp/VNode/VNode.hpp
@@ -1,24 +1,17 @@
 #ifndef VNode_hpp
 #define VNode_hpp
 
-class VNode {
-  char* sel;
-  char* key;
-  // char* text;
+#include <emscripten/val.h>
+#include <vector>
+#include <string>
+
+struct VNode {
+  std::string sel;
+  std::string key;
+  std::string text;
   // data: VNodeData | undefined;
-  // elm: Node | undefined;
-  // children: Array<VNode | string> | undefined;
-
-  public:
-      VNode (char* selector, char* nodeKey);
-
-      char* getName();
-      void setName(char* selector);
-
-      char* getKey();
-      void setKey(char* nodeKey);
-
-      void render(VNode* node);
+  emscripten::val elm;
+  std::vector<VNode> children;
 };
 
 #endif
