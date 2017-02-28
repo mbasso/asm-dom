e630a44a54f56cc2ede234c7040f3a4f8e4d8e40
mbasso <mbasso96@gmail.com>
2017-02-28 18:33:25 +0100

    Fix h header

--- src/cpp/H/h.hpp
+++ src/cpp/H/h.hpp
@@ -10,12 +10,12 @@
 struct VNode* h(const std::string sel);
 struct VNode* h(const std::string text, bool isText);
 
-struct VNode* h(const std::string sel, const val node);
+struct VNode* h(const std::string sel, struct VNode* const node);
 struct VNode* h(const std::string sel, const std::string text);
 struct VNode* h(const std::string sel, struct VNodeData* const data);
 struct VNode* h(const std::string sel, const std::vector<struct VNode*> children);
 
-struct VNode* h(const std::string sel, struct VNodeData* const data, const val node);
+struct VNode* h(const std::string sel, struct VNodeData* const data, struct VNode* const node);
 struct VNode* h(const std::string sel, struct VNodeData* const data, const std::string text);
 struct VNode* h(const std::string sel, struct VNodeData* const data, const std::vector<struct VNode*> children);
 
