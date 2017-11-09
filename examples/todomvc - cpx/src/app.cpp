aadd5fb496cf445ac782c57b8fd11b666cbc1ae6
mbasso <mbasso96@gmail.com>
2017-11-09 19:56:30 +0100

    gccx example

---
+++ examples/todomvc - cpx/src/app.cpp
@@ -0,0 +1,78 @@
+#include "../../../src/cpp/asm-dom.hpp"
+#include "todos.hpp"
+#include <emscripten.h>
+#include <emscripten/bind.h>
+
+using namespace asmdom;
+using namespace todomvc::todos;
+
+std::function<void(todomvc::todos::action)> handler;
+
+void onhashchange(std::string filter) {
+	TodoFilter todoFilter = all;
+	if (filter == "completed") {
+		todoFilter = completed;
+	} else if (filter == "active") {
+		todoFilter = active;
+	}
+	handler(todomvc::todos::Action::Filter(todoFilter));
+};
+
+void render(Todos initState, VNode* oldVnode);
+
+VNode* nextVnode;
+
+VNode* beforePatch(Todos initState) {
+	handler = [initState](std::function<Todos(Todos)> e) {
+		Todos newState = update(initState, e);
+		render(newState, nextVnode);
+	};
+	nextVnode = view(initState, handler);
+	return nextVnode;
+};
+
+void afterPatch(VNode* rendered) {
+	if (rendered != NULL) {
+		EM_ASM(
+			window['onhashchange'] = function() {
+				window['todomvc']['onhashchange'](window['location']['hash']['substr'](2) || 'all');
+			};
+		);
+	}
+};
+
+void render(Todos initState, VNode* oldVnode) {
+	VNode* newVnode = beforePatch(initState);
+	VNode* result = patch(oldVnode, newVnode);
+	afterPatch(result);
+};
+
+void render(Todos initState, emscripten::val oldVnode) {
+	VNode* newVnode = beforePatch(initState);
+	VNode* result = patch(oldVnode, newVnode);
+	afterPatch(result);
+};
+
+int main() {
+	Config config = Config();
+	init(config);
+
+	EM_ASM(
+		window['todomvc'] = {
+			'onhashchange': Module['onhashchange'],
+		};
+	);
+
+	render(
+		todomvc::todos::init(),
+		emscripten::val::global("document").call<emscripten::val>(
+			"querySelector",
+			std::string(".todoapp")
+		)
+	);
+	return 0;
+};
+
+EMSCRIPTEN_BINDINGS(app) {
+  emscripten::function("onhashchange", &onhashchange);
+};
