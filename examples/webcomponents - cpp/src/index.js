8181a8520cf0a71db7e17eb573f46a9f11e07411
mbasso <mbasso96@gmail.com>
2018-04-15 12:07:18 +0200

    fix examples

--- examples/webcomponents - cpp/src/index.js
+++ examples/webcomponents - cpp/src/index.js
@@ -4,20 +4,18 @@ import './components/HelloComponent';
 (() => {
   const config = {};
 
-  new Promise(
-    (resolve) => {
-      if ('WebAssembly' in window) {
-        import('../compiled/wasm/app.wasm')
-                .then((wasm) => {
-                  config.wasmBinary = new Uint8Array(wasm);
-                  return import('../compiled/wasm/app.js');
-                })
-                .then(resolve);
-      } else {
-        import('../compiled/asmjs/app.asm.js').then(resolve);
-      }
-    },
-  )
-    .then(Module => Module(config));
-})();
+  const instantiate = (Module) => {
+    Module(config);
+  };
 
+  if ('WebAssembly' in window) {
+    import('../compiled/wasm/app.wasm')
+            .then((wasm) => {
+              config.wasmBinary = new Uint8Array(wasm);
+              return import('../compiled/wasm/app.js');
+            })
+            .then(instantiate);
+  } else {
+    import('../compiled/asmjs/app.asm.js').then(instantiate);
+  }
+})();
